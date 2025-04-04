6c319bbc0586c5f43b7a5421cb100a1392b32adb
Tejas Garg <tejasgarg8421@gmail.com>
2025-04-05 00:08:25 +0530

    fix: fixing function

--- combine_hex.sh
+++ combine_hex.sh
@@ -11,7 +11,7 @@ srec_cat \
   $FIRMWARE_HEX -intel -crop 0x3000 0x55000 \
   -fill 0xFF 0x3000 0x55000 \
   -offset 0x0000 \
-  $BOOTLOADER_HEX -intel -crop 0x55000 0x100000000 \
-  -fill 0xFF 0x55000 0x100000000 \
+  $BOOTLOADER_HEX -intel -crop 0x55000 0xFFFFFFFF \
+  -fill 0xFF 0x55000 0xFFFFFFFF --big \
   -offset 0x0000 \
   -o $OUTPUT_HEX -intel
