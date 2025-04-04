7b24c92c09fc1b43dd181016d0664099ee6c1a1d
Tejas Garg <tejasgarg8421@gmail.com>
2025-04-04 23:55:37 +0530

    fix: changed combine file for srecord

--- combine_hex.sh
+++ combine_hex.sh
@@ -1,2 +1,17 @@
-#!/bin/sh
-hexmate r0-2FFF,build/pslab-bootloader.hex r3000-54FFF,build/pslab-firmware.hex r55000-FFFFFFFF,build/pslab-bootloader.hex -Obuild/combined.hex
+set -e
+
+BOOTLOADER_HEX="build/pslab-bootloader.hex"
+FIRMWARE_HEX="build/pslab-firmware.hex"
+OUTPUT_HEX="build/combined.hex"
+
+srec_cat \
+  $BOOTLOADER_HEX -intel -crop 0x0000 0x3000 \
+  -fill 0xFF 0x0000 0x3000 \
+  -offset 0x0000 \
+  $FIRMWARE_HEX -intel -crop 0x3000 0x55000 \
+  -fill 0xFF 0x3000 0x55000 \
+  -offset 0x0000 \
+  $BOOTLOADER_HEX -intel -crop 0x55000 0x100000000 \
+  -fill 0xFF 0x55000 0x100000000 \
+  -offset 0x0000 \
+  -o $OUTPUT_HEX -intel
